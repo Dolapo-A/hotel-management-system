3c35ac45d6924ccbda04374a80417d12453bdd8b
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2025-03-09 15:17:41 +0100

    Refactor UI for mobile screens and improve responsive message

--- src/pages/MobileScreen.jsx
+++ src/pages/MobileScreen.jsx
@@ -4,23 +4,26 @@ const StyledMobileScreen = styled.div`
 	display: flex;
 	height: 100vh;
 	align-items: center;
-    justify-content: center;
-    padding: 3rem;
-    background-color: var(--color-brand-600);
-    color: var(--color-grey-0);
-    text-align: center;
+	justify-content: center;
+	padding: 3rem;
+	background-color: var(--color-brand-600);
+	color: var(--color-grey-0);
+	text-align: center;
 `;
 
 function MobileScreen() {
-    return (
-        <>
-            <StyledMobileScreen>
-                <div>
-                    For the best experience, please use a device with a larger screen 😁. We recommend a width of at least 1024px.
-                </div>
-            </StyledMobileScreen>
-        </>
-    );
+	return (
+		<>
+			<StyledMobileScreen>
+				<div>
+					<h1>
+						For the best experience, please use a device with a larger screen
+						😁. We recommend a width of at least 1024px.
+					</h1>
+				</div>
+			</StyledMobileScreen>
+		</>
+	);
 }
 
 export default MobileScreen;
